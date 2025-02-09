a78f3eb29bf46d169059cacad6c4c62cd95642e5
coderRaj07 <12131501.edu@gmail.com>
2025-02-09 20:03:35 +0530

    feat: data validation added

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -16,17 +16,17 @@ training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
 
 @dataclass
 class DataIngestionConfig:
-    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)    # artifacts/02_09_2025_14_59_36
-    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME) # artifacts/02_09_2025_14_59_36/feature_store/data.csv
-    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME) # artifacts/02_09_2025_14_59_36/ingested/train.csv
-    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME) # artifacts/02_09_2025_14_59_36/ingested/test.csv
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)    # artifact/02_09_2025_14_59_36
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME) # artifact/02_09_2025_14_59_36/feature_store/data.csv
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME) # artifact/02_09_2025_14_59_36/ingested/train.csv
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME) # artifact/02_09_2025_14_59_36/ingested/test.csv
     train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO # 0.25
     collection_name:str = DATA_INGESTION_COLLECTION_NAME # insurance_data
 
 @dataclass
 class DataValidationConfig:
-    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
-    validation_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_REPORT_FILE_NAME)
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)  # artifact/02_09_2025_14_59_36/data_validation/
+    validation_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_REPORT_FILE_NAME) # artifact/02_09_2025_14_59_36/report.yaml
 
 @dataclass
 class DataTransformationConfig:
